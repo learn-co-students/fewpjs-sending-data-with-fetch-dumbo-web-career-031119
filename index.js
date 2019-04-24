3b394832e2768ad90ff537aebd0d892a010edd8c
github username <github email address>
2019-04-24 14:31:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Add your code here
+
+  let formData = {
+    name: name,
+    email: email
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+function submitData(name,email){
+  let formData = {
+    name: name,
+    email: email
+  }
+
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+
+  return fetch('http://localhost:3000/users',configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(parsedResponse){
+    document.body.innerHTML = parsedResponse["id"]
+  })
+  .catch(function(error){
+    alert("something went wrong dummy!");
+  document.body.innerHTML = error.message;
+      })
+  }
