eb52bcd02df1816433893c33e545c8c4c70b8471
siberry <siberry226@gmail.com>
2019-04-25 12:24:53 -0400

    Done.

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function submitData( name, email ) {
         email
       } )
     } )
-    .then( function ( response ) {
-      return response.json()
-    } )
-    .then( function ( object ) {
+    .then( response =>
+      {return response.json()}
+     )
+    .then( object => {
       document.body.innerHTML = object[ "id" ]
     } )
-    .catch( function ( error ) {
+    .catch( error => {
       document.body.innerHTML = error.message
     } )
 }
