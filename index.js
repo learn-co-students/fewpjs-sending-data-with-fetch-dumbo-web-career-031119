1db7852578ec9087ddb39156a681dacd9e543508
sabby155 <nebula155@gmail.com>
2019-04-28 16:45:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+
+
+
+const submitData = (name, email) => {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email 
+        })
+    }).then(resp => resp.json()).then(parsedData => {
+        renderData([parsedData])
+    }).catch(error => {
+        document.body.innerHTML = error.message;
+    });
+}
+
+const renderData = (parsedData) => {
+    parsedData.forEach(element => {
+
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML += `${element.id}`
+        document.body.append(newDiv);
+    });
+}
