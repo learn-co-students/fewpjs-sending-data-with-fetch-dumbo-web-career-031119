31c93df2e0b7a603920149bcf0eca62246200e73
erodxd <emrod718@gmail.com>
2019-05-03 11:48:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+const submitData = (name, email) => {
+    let formData = {
+        name: name,
+        email: email
+    }
+
+    let object = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    };
+    return fetch('http://localhost:3000/users', object)
+        .then(res => res.json())
+        .then(parsedRes => {
+            return document.body.innerHTML = parsedRes['id']
+        })
+        .catch(error => {
+            alert('Unauthorized Access');
+            document.body.innerHTML = error.message;
+        });
+}
